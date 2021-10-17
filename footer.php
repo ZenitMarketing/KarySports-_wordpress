bc0c6d705aac4e22d49cba816165ef17d5e1c3b7
Oscar Pérez <oscar.pay4@gmail.com>
2021-10-17 13:46:24 -0500

    add basic kary sports layout

--- footer.php
+++ footer.php
@@ -1,52 +1,44 @@
-<footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-6 footer__contact margin-bottom-xl">
-        <h4 class="footer__title">Contáctanos</h4>
-
-        <p class="footer__text">La estrategia de marketing digital que tu negocio necesita</p>
-
-        <div class="footer__contact__info">
-          <i class="fa fa-home"></i> Mérida, Yucatán
+  <footer>
+    <div class="footer__top">
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <img src="<?php bloginfo('template_directory') ?>/images/logo_footer.png" alt="logo">
+          </div>
+          <div class="col-3 footer__section">
+            <h3 class="footer_title">Nosotros</h3>
+  
+            <p class="footer_text">Quienes somos</p>
+  
+            <p class="footer_text">Preguntas frecuentes</p>
+          </div>
+          <div class="col-3 footer__section">
+            <h3 class="footer_title">Servicio al cliente</h3>
+  
+            <p class="footer_text">Contáctanos</p>
+  
+            <p class="footer_text">Aviso de privacidad</p>
+          </div>
+          <div class="col-3 footer__section">
+            <h3 class="footer_title">Métodos de pago</h3>
+  
+            <div class="cards-container">
+              <img class="visa-img" src="<?php bloginfo('template_directory') ?>/images/visa.png" alt="logo">
+              <img src="<?php bloginfo('template_directory') ?>/images/mastercard.png" alt="logo">
+            </div>
+          </div>
         </div>
-        <div class="footer__contact__info">
-          <i class="fa fa-phone"></i> Tel: <a href="tel:9992 627902">9992 627902</a>
-        </div>
-        <div class="footer__contact__info">
-          <i class="fa fa-envelope"></i> Email: <a href="mailto:contactzenit.mkt@gmail.com">contactzenit.mkt@gmail.com</a>
-        </div>
-      </div>
-      <div class="col-12 col-lg-2 margin-bottom-xl">
-        <h4 class="footer__title">Paquetes</h4>
-
-        <ul class="footer__links">
-          <li><a href="#">Paquete inicial</a></li>
-          <li><a href="#">Paquete recomendado</a></li>
-          <li><a href="#">Paquete profesional</a></li>
-        </ul>
-      </div>
-      <div class="col-12 col-lg-2 margin-bottom-xl">
-        <h4 class="footer__title">Otros Servicios</h4>
-
-        <ul class="footer__links">
-          <li><a href="#">Fotografía y Vídeo</a></li>
-          <li><a href="#">Email Marketing</a></li>
-          <li><a href="#">Conexión con proveedores</a></li>
-          <li><a href="#">Branding</a></li>
-        </ul>
-      </div>
-      <div class="col-12 col-lg-2">
-        <h4 class="footer__title">Manada Zenit</h4>
-
-        <ul class="footer__links">
-          <li><a href="#">Nuestro equipo</a></li>
-          <li><a href="#">Únete a la manada</a></li>
-          <li><a href="#">Aviso de privacidad</a></li>
-        </ul>
       </div>
     </div>
-  </div>
-</footer>
+
+    <div class="footer__bottom">
+      <div class="container">
+        <div class="row footer__bottom-container">
+          Desarrollado por Zenit Marketing para Kary Sports Wear Store 2021
+        </div>
+      </div>
+    </div>
+  </footer>
   
   <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -12,146 +12,49 @@
 <body>
 
 <header class="header">
-
-    <nav>
-      <div class="d-flex justify-content-between align-items-center container">
-        <a href="/" class="header__logo">
-          <img src="<?php bloginfo('template_directory') ?>/images/zenit_text_logo.png" alt="zenit logo">
-        </a>
-
-        <a id="btnHamburger" href="#" class="header__toggle d-block d-lg-none">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
-
-        <?php 
-          wp_nav_menu(
-            array(
-              'theme_location' => 'top_menu',
-              'menu' => 'main-menu',
-              'container' => 'ul',
-              'menu_class' => 'header__links d-none d-lg-block'
-            )
-          )
-        ?>
-
-      </div>
-    </nav>
-
-    <div class="header__menu has-fade">
-
-      <?php 
-        wp_nav_menu(
-          array(
-            'theme_location' => 'top_menu',
-            'menu' => 'mobile-menu',
-            'container' => 'ul',
-            'menu_class' => 'links-mobile'
-          )
-        )
-      ?>
-
+  <div class="header__top">
+    <div class="container">
+      <p>Envíos a toda la republica desde $160 - Envio gratis en compras mayores a $1399</p>
     </div>
-  </header>
-  
- <!--  <header class="header">
+  </div>
 
-    <div class="overlay has-fade"></div>
+  <div class="overlay has-fade"></div>
 
-    <div class="socials d-none d-lg-block">
-      <div class="container d-flex justify-content-end">
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/face.svg" alt="mail icon">
-        </a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/insta.svg" alt="mail icon">
-        </a>
-        <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/whats.svg" alt="mail icon">
-        </a>
-        <a href="mailto:netfishmexico@gmail.com">
-          <img src="<?php bloginfo('template_directory') ?>/images/mail.svg" alt="mail icon">
-        </a>
+  <nav>
+    <div class="d-flex justify-content-between align-items-end container">
+      <a href="/" class="header__logo">
+        <img src="<?php bloginfo('template_directory') ?>/images/logo.png" alt="kary sports logo">
+      </a>
 
-        <?php 
-          wp_nav_menu(
-            array(
-              'theme_location' => 'languages_menu',
-              'menu' => 'languages-menu',
-              'container' => 'ul',
-              'menu_class' => 'lang-links'
-            )
-          )
-        ?>
-      </div>
+      <a id="btnHamburger" href="#" class="header__toggle d-block d-lg-none">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+
+      <ul class="header__links d-none d-lg-block">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Nosotros</a></li>
+        <li><a href="#">Tienda</a></li>
+        <li><a href="#">FAQ'S</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><i class="fa fa-user"></i></li>
+        <li><i class="fas fa-shopping-cart"></i></li>
+      </ul>
     </div>
+  </nav>
 
-    <nav>
-      <div class="d-flex justify-content-between align-items-center container">
-        <a href="/" class="header__logo">
-          <img src="<?php bloginfo('template_directory') ?>/images/logo-white.png" alt="Logo">
-        </a>
-  
-        <a id="btnHamburger" href="#" class="header__toggle d-block d-lg-none">
-          <span></span>
-          <span></span>
-          <span></span>
-        </a>
+  <div class="header__menu has-fade">
 
-        <?php 
-          wp_nav_menu(
-            array(
-              'theme_location' => 'top_menu',
-              'menu' => 'main-menu',
-              'container' => 'ul',
-              'menu_class' => 'header__links d-none d-lg-block'
-            )
-          )
-        ?>
-  
-      </div>
-    </nav>
+    <ul class="links-mobile">
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Nosotros</a></li>
+      <li><a href="#">Tienda</a></li>
+      <li><a href="#">FAQ'S</a></li>
+      <li><a href="#">Contacto</a></li>
+      <li><i class="fa fa-user"></i></li>
+      <li><i class="fas fa-shopping-cart"></i></li>
+    </ul>
 
-    <div class="header__menu has-fade">
-
-      <?php 
-        wp_nav_menu(
-          array(
-            'theme_location' => 'top_menu',
-            'menu' => 'mobile-menu',
-            'container' => 'ul',
-            'menu_class' => 'links-mobile'
-          )
-        )
-      ?>
-
-      <div class="mobile-socials d-flex justify-content-center">
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/face.svg" alt="mail icon">
-        </a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/insta.svg" alt="mail icon">
-        </a>
-        <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
-          <img src="<?php bloginfo('template_directory') ?>/images/whats.svg" alt="mail icon">
-        </a>
-        <a href="mailto:netfishmexico@gmail.com">
-          <img src="<?php bloginfo('template_directory') ?>/images/mail.svg" alt="mail icon">
-        </a>
-      </div>
-
-      <div class="d-flex justify-content-center">
-        <?php 
-          wp_nav_menu(
-            array(
-              'theme_location' => 'languages_menu',
-              'menu' => 'languages-menu',
-              'container' => 'ul',
-              'menu_class' => 'lang-links'
-            )
-          )
-        ?>
-      </div>
-    </div>
-  </header> -->
+  </div>
+</header>
